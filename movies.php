a8e52e25cb70bea4b3aceb482005200cedc7759e
unknown <it18-9@M306.uc-bcf.edu.ph>
2019-03-19 13:53:00 +0800

    update

---
+++ movies.php
@@ -0,0 +1,42 @@
+<!-- header -- >
+<?php include 'sections/header.php'; ?>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	<link href="./css/style.css" rel="stylesheet">
+
+<style>
+	#banner{
+		height: 250px;
+		background-image: url("../img/background.png");
+	}
+	#title p{
+		color: white;
+		background-color: red;
+	}
+
+
+</style>
+
+
+<!--- Jumbotron -->
+<div class="container-fluid">
+	<div class="row jumbotron" id="banner">
+		<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10" id="title">
+			<p class="lead">MOVIES</p>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+<!--- Footer -->
+<?php include 'sections/footer.php'; ?>
+
+</body>
+</html>
---
+++ sections/movies.php
@@ -0,0 +1,22 @@
+<!-- header -- >
+<?php include 'sections/header.php'; ?>
+
+
+<!--- Jumbotron -->
+<div class="container-fluid">
+	<div class="row jumbotron">
+		<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
+			<p class="lead">Movies Insert Picture</p>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+<!--- Footer -->
+<?php include 'sectiona/footer.php'; ?>
+
+</body>
+</html>
